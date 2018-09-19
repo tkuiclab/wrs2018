8150b4b43220e4da3f0fb6d62b29009c4fcac6ad
youjun <st99315@yahoo.com.tw>
2018-09-20 06:20:43 +0800

    Update objectPos.py for More Clear Code

--- strategy/src/objectPos.py
+++ strategy/src/objectPos.py
@@ -1,24 +1,10 @@
 #!/usr/bin/env python
 
-"""Use to generate arm task and run."""
+"""Use to reset pose of model for simulation."""
 
-import os
-import sys
 import rospy
-import rospkg
 from gazebo_msgs.msg import ModelState
 
-
-set_mode_pub = rospy.Publisher(
-    '/gazebo/set_model_state',
-    ModelState,
-    queue_size=1,
-    latch=True
-)
-
-def set_pub():
-    print "set_link_state "
-    global set_mode_pub
 
 def set_object(name, pos, ori):
     msg = ModelState()
@@ -31,12 +17,22 @@ def set_object(name, pos, ori):
     msg.pose.orientation.y = ori[2]
     msg.pose.orientation.z = ori[3]
     msg.reference_frame = 'world'
+    
     set_mode_pub.publish(msg)
     print msg
 
+    
 if __name__ == '__main__':
     rospy.init_node('set_obj')
-    set_pub()
+    print("set_link_state")
+    
+    set_mode_pub = rospy.Publisher(
+        '/gazebo/set_model_state',
+        ModelState,
+        queue_size=1,
+        latch=True
+    )
+
     names = ('lunchbox', 'lunchbox1', 'lunchbox2')
     pos  = ((-0.6, -0.166, 0.7),
             (-0.6, -0.166, 0.75),
@@ -44,6 +40,7 @@ if __name__ == '__main__':
     ori  = ((0, 0, 0, 1),
             (0, 0, 0, 1),
             (0, 0, 0, 1))
+    
     for i, name in enumerate(names):
         set_object(name, pos[i], ori[i])
         rospy.sleep(1)
